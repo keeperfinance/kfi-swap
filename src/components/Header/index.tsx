f07679b180a6dc2e3b1395dbd03c35725868f4ac
Sol Proc <info@kp2r.network>
2020-12-15 10:50:08 +0600

    swap

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,268 @@
+import { ChainId } from '@uniswap/sdk'
+import React from 'react'
+import { isMobile } from 'react-device-detect'
+import { Text } from 'rebass'
+
+import styled from 'styled-components'
+import { NavLink } from 'react-router-dom'
+import { darken } from 'polished'
+
+import Logo from '../../assets/svg/wordmark.svg'
+import LogoDark from '../../assets/svg/wordmark_white.svg'
+import { useActiveWeb3React } from '../../hooks'
+import { useDarkModeManager } from '../../state/user/hooks'
+import { useETHBalances } from '../../state/wallet/hooks'
+
+import { YellowCard } from '../Card'
+import Settings from '../Settings'
+import Menu from '../Menu'
+
+import { RowBetween } from '../Row'
+import Web3Status from '../Web3Status'
+import { ExternalLink } from '../../theme'
+
+const HeaderFrame = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  top: 0;
+  position: absolute;
+  z-index: 2;
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    padding: 12px 0 0 0;
+    width: calc(100%);
+    position: relative;
+  `};
+`
+
+const HeaderElement = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const HeaderElementWrap = styled.div`
+  display: flex;
+  align-items: center;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    margin-top: 0.5rem;
+`};
+`
+const HeaderMob = styled.div`
+  display: contents;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    display: grid;
+    justify-content: center;
+    width: 100%;
+`};
+`
+const HeaderNav = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    margin-top: 0.5rem;
+`};
+`
+
+const Title = styled.a`
+  display: flex;
+  align-items: center;
+  pointer-events: auto;
+
+  :hover {
+    cursor: pointer;
+  }
+`
+const AccountElement = styled.div<{ active: boolean }>`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: ${({ theme, active }) => (!active ? theme.bg1 : theme.bg3)};
+  border-radius: 12px;
+  white-space: nowrap;
+  width: 100%;
+
+  :focus {
+    border: 2px solid blue;
+  }
+`
+
+const TestnetWrapper = styled.div`
+  white-space: nowrap;
+  width: fit-content;
+  margin-left: 10px;
+  pointer-events: auto;
+`
+
+const NetworkCard = styled(YellowCard)`
+  width: fit-content;
+  margin-right: 10px;
+  border-radius: 12px;
+  padding: 8px 12px;
+`
+
+const activeClassName = 'ACTIVE'
+
+const StyledNavLink = styled(NavLink).attrs({
+  activeClassName
+})`
+  ${({ theme }) => theme.flexRowNoWrap}
+  align-items: left;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text2};
+  font-size: 1.2rem;
+  width: fit-content;
+  margin: 0 12px;
+  font-weight: 500;
+  &.${activeClassName} {
+    font-weight: 600;
+    color: ${({ theme }) => theme.text1};
+    border-bottom: 3px solid #1C71E4;
+  }
+  :hover,
+  :focus {
+    color: ${({ theme }) => darken(0.1, theme.text1)};
+    border-bottom: 3px solid #394053;
+    text-decoration: none;
+  }
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+      margin: 0 4px;
+`}
+`
+
+const StyledExternalLink = styled(ExternalLink).attrs({
+  activeClassName
+})<{ isActive?: boolean }>`
+  ${({ theme }) => theme.flexRowNoWrap}
+  align-items: left;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
+  color: ${({ theme }) => theme.text2};
+  font-size: 1.2rem;
+  width: fit-content;
+  margin: 0 12px;
+  font-weight: 500;
+  &.${activeClassName} {
+    font-weight: 600;
+    color: ${({ theme }) => theme.text1};
+    border-bottom: 3px solid #1C71E4;
+  }
+  :hover,
+  :focus {
+    color: ${({ theme }) => darken(0.1, theme.text1)};
+    border-bottom: 3px solid #394053;
+    text-decoration: none;
+  }
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+      margin: 0 4px;
+`}
+`
+
+const UniIcon = styled.div`
+  transition: transform 0.3s ease;
+  :hover {
+    transform: rotate(-5deg);
+  }
+`
+
+const HeaderControls = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  ${({ theme }) => theme.mediaWidth.upToSmall`
+    align-items: flex-end;
+    padding-bottom: 20px;
+  `};
+`
+
+const BalanceText = styled(Text)`
+  ${({ theme }) => theme.mediaWidth.upToExtraSmall`
+    display: flex;
+  `};
+`
+
+const NETWORK_LABELS: { [chainId in ChainId]: string | null } = {
+  [ChainId.MAINNET]: null,
+  [ChainId.RINKEBY]: 'Rinkeby',
+  [ChainId.ROPSTEN]: 'Ropsten',
+  [ChainId.GÖRLI]: 'Görli',
+  [ChainId.KOVAN]: 'Kovan'
+}
+
+export default function Header() {
+  const { account, chainId } = useActiveWeb3React()
+
+  const userEthBalance = useETHBalances(account ? [account] : [])?.[account ?? '']
+  const [isDark] = useDarkModeManager()
+
+  return (
+    <HeaderFrame>
+      <RowBetween style={{ alignItems: 'flex-start' }} padding="1rem 1rem 0 1rem">
+      <HeaderMob>
+        <HeaderElement>
+          <Title href=".">
+            <UniIcon>
+              <img src={isDark ? LogoDark : Logo} alt="logo" height="32px" />
+            </UniIcon>
+            </Title>
+        </HeaderElement>
+        <HeaderNav>
+        <StyledNavLink id={`swap-nav-link`} to={'/swap'}>
+           Swap
+         </StyledNavLink>
+         <StyledNavLink
+           id={`pool-nav-link`}
+           to={'/pool'}
+           isActive={(match, { pathname }) =>
+             Boolean(match) ||
+             pathname.startsWith('/add') ||
+             pathname.startsWith('/remove') ||
+             pathname.startsWith('/create') ||
+             pathname.startsWith('/find')
+           }
+         >
+           Pool
+         </StyledNavLink>
+         <StyledExternalLink id={`stake-nav-link`} href={'https://app.keeper.finance'}>
+           Keeper
+         </StyledExternalLink>
+         <StyledExternalLink id={`stake-nav-link`} href={'/'}>
+           Vote
+         </StyledExternalLink>
+         <StyledExternalLink id={`stake-nav-link`} href={'/'}>
+           Charts
+         </StyledExternalLink>
+        </HeaderNav>
+        <HeaderControls>
+          <HeaderElement>
+            <TestnetWrapper>
+              {!isMobile && chainId && NETWORK_LABELS[chainId] && <NetworkCard>{NETWORK_LABELS[chainId]}</NetworkCard>}
+            </TestnetWrapper>
+            <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
+              {account && userEthBalance ? (
+                <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
+                  {userEthBalance?.toSignificant(4)} ETH
+                </BalanceText>
+              ) : null}
+              <Web3Status />
+            </AccountElement>
+          </HeaderElement>
+          <HeaderElementWrap>
+            <Settings />
+            <Menu />
+          </HeaderElementWrap>
+        </HeaderControls>
+        </HeaderMob>
+      </RowBetween>
+    </HeaderFrame>
+  )
+}
---
+++ src/theme/index.tsx
@@ -0,0 +1,218 @@
+import React, { useMemo } from 'react'
+import styled, {
+  ThemeProvider as StyledComponentsThemeProvider,
+  createGlobalStyle,
+  css,
+  DefaultTheme
+} from 'styled-components'
+import { useIsDarkMode } from '../state/user/hooks'
+import { Text, TextProps } from 'rebass'
+import { Colors } from './styled'
+
+export * from './components'
+
+const MEDIA_WIDTHS = {
+  upToExtraSmall: 500,
+  upToSmall: 600,
+  upToMedium: 960,
+  upToLarge: 1280
+}
+
+const mediaWidthTemplates: { [width in keyof typeof MEDIA_WIDTHS]: typeof css } = Object.keys(MEDIA_WIDTHS).reduce(
+  (accumulator, size) => {
+    ;(accumulator as any)[size] = (a: any, b: any, c: any) => css`
+      @media (max-width: ${(MEDIA_WIDTHS as any)[size]}px) {
+        ${css(a, b, c)}
+      }
+    `
+    return accumulator
+  },
+  {}
+) as any
+
+const white = '#FFFFFF'
+const black = '#000000'
+
+export function colors(darkMode: boolean): Colors {
+  return {
+    // base
+    white,
+    black,
+
+    // text
+    text1: darkMode ? '#FFFFFF' : '#000000',
+    text2: darkMode ? '#C6D2E2' : '#4C5E76',
+    text3: darkMode ? '#9699a2' : '#888D9B',
+    text4: darkMode ? '#4C5E76' : '#C6D2E2',
+    text5: darkMode ? '#131B26' : '#fff8fb',
+
+    // backgrounds / greys
+    bg1: darkMode ? '#1B2738' : '#FFFFFF',
+    bg2: darkMode ? '#131B26' : '#e7ebf4',
+    bg3: darkMode ? '#28374C' : '#C6D2E2',
+    bg4: darkMode ? '#4C5E76' : '#D6DCDD',
+    bg5: darkMode ? '#9699a2' : '#888D9B',
+    bg6: darkMode ? '#1C71E4' : '#e7ebf4',
+
+    //specialty colors
+    modalBG: darkMode ? 'rgba(0,0,0,.425)' : 'rgba(0,0,0,0.3)',
+    advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
+    border: darkMode ? 'rgba(42, 57, 79, 0.9)' : 'rgba(197, 210, 237 , 0.9)',
+
+    //primary colors
+    primary1: darkMode ? '#1C71E4' : '#1C71E4',
+    primary2: darkMode ? '#3680E7' : '#FF8CC3',
+    primary3: darkMode ? '#4D8FEA' : '#FF99C9',
+    primary4: darkMode ? '#28374C70' : '#deecff50',
+    primary5: darkMode ? '#3f4458' : '#EEF5FF',
+
+    // color text
+    primaryText1: darkMode ? '#1C71E4' : '#1C71E4',
+
+    // secondary colors
+    secondary1: darkMode ? '#1C71E4' : '#1C71E4',
+    secondary2: darkMode ? '#17000b26' : '#06938550',
+    secondary3: darkMode ? '#17000b26' : '#EEFFFD',
+
+    // other
+    red1: '#FF6871',
+    red2: '#F82D3A',
+    green1: '#27AE60',
+    yellow1: '#FFE270',
+    yellow2: '#F3841E'
+
+    // dont wanna forget these blue yet
+    // blue4: darkMode ? '#218a8160' : '#C4D9F8',
+    // blue5: darkMode ? '#218a8160' : '#EBF4FF',
+  }
+}
+
+export function theme(darkMode: boolean): DefaultTheme {
+  return {
+    ...colors(darkMode),
+
+    grids: {
+      sm: 8,
+      md: 12,
+      lg: 24
+    },
+
+    //shadows
+    shadow1: darkMode ? '#000' : '#2F80ED',
+
+    // media queries
+    mediaWidth: mediaWidthTemplates,
+
+    // css snippets
+    flexColumnNoWrap: css`
+      display: flex;
+      flex-flow: column nowrap;
+    `,
+    flexRowNoWrap: css`
+      display: flex;
+      flex-flow: row nowrap;
+    `
+  }
+}
+
+export default function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const darkMode = useIsDarkMode()
+
+  const themeObject = useMemo(() => theme(darkMode), [darkMode])
+
+  return <StyledComponentsThemeProvider theme={themeObject}>{children}</StyledComponentsThemeProvider>
+}
+
+const TextWrapper = styled(Text)<{ color: keyof Colors }>`
+  color: ${({ color, theme }) => (theme as any)[color]};
+`
+
+export const TYPE = {
+  main(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'text2'} {...props} />
+  },
+  link(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'primary1'} {...props} />
+  },
+  black(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'text1'} {...props} />
+  },
+  body(props: TextProps) {
+    return <TextWrapper fontWeight={400} fontSize={20} color={'text1'} {...props} />
+  },
+  largeHeader(props: TextProps) {
+    return <TextWrapper fontWeight={600} fontSize={28} {...props} />
+  },
+  mediumHeader(props: TextProps) {
+    return <TextWrapper fontWeight={500} fontSize={24} {...props} />
+  },
+  subHeader(props: TextProps) {
+    return <TextWrapper fontWeight={400} fontSize={18} {...props} />
+  },
+  blue(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'primary1'} {...props} />
+  },
+  yellow(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'yellow1'} {...props} />
+  },
+  darkGray(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'text3'} {...props} />
+  },
+  gray(props: TextProps) {
+    return <TextWrapper fontWeight={500} color={'bg3'} {...props} />
+  },
+  italic(props: TextProps) {
+    return <TextWrapper fontWeight={500} fontSize={18} fontStyle={'italic'} color={'text2'} {...props} />
+  },
+  error({ error, ...props }: { error: boolean } & TextProps) {
+    return <TextWrapper fontWeight={500} color={error ? 'red1' : 'text2'} {...props} />
+  }
+}
+
+export const FixedGlobalStyle = createGlobalStyle`
+html, input, textarea, button {
+  font-family: 'Teko', sans-serif;
+  letter-spacing: -0.018em;
+  font-display: fallback;
+}
+@supports (font-variation-settings: normal) {
+  html, input, textarea, button {
+    font-family: 'Teko', sans-serif;
+  }
+}
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+button {
+  user-select: none;
+}
+
+html {
+  font-size: 16px;
+  font-variant: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+}
+`
+
+export const ThemedGlobalStyle = createGlobalStyle`
+html {
+  color: ${({ theme }) => theme.text1};
+  background-color: ${({ theme }) => theme.bg2};
+}
+
+body {
+  min-height: 100vh;
+  background-position: 0 -30vh;
+  background-repeat: no-repeat;
+}
+`
